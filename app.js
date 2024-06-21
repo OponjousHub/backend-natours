13a7ce20729caf48af03f7b3184629b5a3af8adc
Oponjous <joe.ikdura@gmail.com>
2024-06-21 13:46:32 +0100

    Added global error handler with express

--- app.js
+++ app.js
@@ -34,9 +34,24 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'Fail',
-    message: `can't find the ${req.originalUrl} on the server!`,
+  // res.status(404).json({
+  //   status: 'Fail',
+  //   message: `can't find the ${req.originalUrl} on the server!`,
+  // });
+  const err = new Error(`can't find the ${req.originalUrl} on the server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 

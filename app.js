c25fe3ed9aa8e3630c335896ef0b57b8dac2bd37
Oponjous <joe.ikdura@gmail.com>
2024-06-21 14:28:20 +0100

    creating a central class for all errors and refactoring

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const tourRouter = require('./route/tourRoutes');
 const userRouter = require('./route/userRoutes');
+const AppError = require('./utils/appError');
+const globalerrorHandler = require('./controllers/errorController');
 
 ///////////////// MIDDLEWARES ///////////////////
 
@@ -34,26 +36,14 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'Fail',
-  //   message: `can't find the ${req.originalUrl} on the server!`,
-  // });
-  const err = new Error(`can't find the ${req.originalUrl} on the server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
+  // const err = new Error(`can't find the ${req.originalUrl} on the server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
-  next(err);
+  next(new AppError(`can't find the ${req.originalUrl} on the server!`));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalerrorHandler);
 
 ///////////////// STARTING SeRVER///////////////////
 

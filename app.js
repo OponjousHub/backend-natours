e1ef2823506c3464cae140f57525cff0ce12b8bf
Oponjous <joe.ikdura@gmail.com>
2024-08-13 15:01:46 +0100

    Added rate or request limiting

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const tourRouter = require('./route/tourRoutes');
 const userRouter = require('./route/userRoutes');
 const AppError = require('./utils/appError');
 const globalerrorHandler = require('./controllers/errorController');
+const rateLimit = require('express-rate-limit');
 
 ///////////////// MIDDLEWARES ///////////////////
 
 const app = express();
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);

df3c27b1561a221e82186c353dc71e8dc4b55d90
Oponjous <joe.ikdura@gmail.com>
2024-08-30 16:29:25 +0100

    Calculating the rating average if review is updated

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -57,15 +57,30 @@ reviewSchema.statics.calcAverargeRatings = async function (tourId) {
   ]);
   console.log(stats);
 
-  await Tour.findByIdAndUpdate(tourId, {
-    ratingsQuantity: stats[0].nRating,
-    ratingsAverage: stats[0].avgRating,
-  });
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
 };
 
 // Calling above function to calculate the average review
 reviewSchema.post('save', function () {
   this.constructor.calcAverargeRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne();
+  console.log(this.r);
+});
+reviewSchema.post(/^findOneAnd/, async function () {
+  await this.r.constructor.calcAverargeRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);

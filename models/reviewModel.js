3528d09be5c6eb00070e7db49eb21e605e7d0ac2
Oponjous <joe.ikdura@gmail.com>
2024-08-30 15:18:19 +0100

    Calculating the rating average and rating quantity

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -38,6 +39,33 @@ reviewSchema.pre(/^find/, function (next) {
     select: 'name photo',
   });
   next();
+});
+
+// This will be called antime a review is saved or updated
+reviewSchema.statics.calcAverargeRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  console.log(stats);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].nRating,
+    ratingsAverage: stats[0].avgRating,
+  });
+};
+
+// Calling above function to calculate the average review
+reviewSchema.post('save', function () {
+  this.constructor.calcAverargeRatings(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);

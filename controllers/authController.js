26265f45f2f02321a181028374e9d1e8c4c0a481
Oponjous <joe.ikdura@gmail.com>
2024-09-19 13:30:21 +0100

    Logging in user with api

--- controllers/authController.js
+++ controllers/authController.js
@@ -71,9 +71,9 @@ exports.protect = catchAsyncError(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
-  console.log(token);
-  console.log(req.headers.authorization);
   if (!token) {
     return next(new AppError('You are not logged in! Please log in.', 401));
   }
--- route/viewRoutes.js
+++ route/viewRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const viewControllers = require('../controllers/viewControllers');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
 router.get('/', viewControllers.getOverview);
-router.get('/tour/:slug', viewControllers.getTours);
+router.get('/tour/:slug', authController.protect, viewControllers.getTours);
 router.get('/login', viewControllers.getLoginForm);
 
 module.exports = router;

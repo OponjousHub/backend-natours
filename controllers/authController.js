0b040ead1647e058613a92eac48bbd1a252a1585
Oponjous <joe.ikdura@gmail.com>
2024-11-08 10:05:21 +0100

    adjusted code to test whether connection is secure

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,7 +21,7 @@ const createSendToken = (user, statusCode, res) => {
     ),
     httpOnly: true,
   };
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  if (req.secure) cookieOptions.secure = true;
   res.cookie('jwt', token, cookieOptions);
 
   // Remove the password from the output
